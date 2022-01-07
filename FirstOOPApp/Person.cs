01967e374b2f16bca6d783f0118b443982f94bd3
Tarlan Usubov <tarlanru@code.edu.az>
2022-01-07 21:24:28 +0400

    Initial commit

---
+++ FirstOOPApp/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstOOPApp
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public byte Age { get; set; }
+        public bool Gender { get; set; }
+
+        private decimal Salary;
+
+
+        public string IntroduceYourself()
+        {
+            string result = "";
+
+            result += "Mənim adım " + Name;
+            result += ", mənim soyadım " + Surname;
+            result += ", mənim yaşım isə " + Age;
+
+            return result;
+        }
+
+
+    }
+}
---
+++ FirstOOPApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FirstOOPApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Terlan";
+
+            Person person1 = new Person();
+
+            person1.Name = "Terlan";
+            person1.Surname = "Usubov";
+            person1.Age = 22;
+
+
+            string introduce = person1.IntroduceYourself();
+
+            Console.WriteLine(introduce);
+        }
+    }
+}
